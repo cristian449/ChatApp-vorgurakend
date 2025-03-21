806c00bccbc8f6f4d96e23b449502837fb4a61cf
cristian449 <144561600+cristian449@users.noreply.github.com>
2025-03-21 09:37:19 +0200

    Initial Commit
    Had to copy old files from the old project as the files were not being commited and pushed to github, i just create a new repo and copied all the filed i had done so far and pasted them into this repository

---
+++ ChatApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatClient.MVVM.Core;
+using ChatClient.Net;
+
+namespace ChatClient.MVVM.ViewModel
+{
+    class MainViewModel
+    {
+        public RelayCommand ConnectToServerCommand { get; set; }
+
+        public string Username { get; set; }
+
+        private Server _server;
+        public MainViewModel()
+        {
+            _server = new Server();
+            ConnectToServerCommand = new RelayCommand(o => _server.ConnectToServer(Username));
+        }
+
+    }
+}
---
+++ ChatServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ChatServer
+{
+    class Program
+    {
+        static List<Client> _users;
+        static TcpListener _listner;
+        static void Main(string[] args)
+        {
+            _users = new List<Client>();
+            _listner = new TcpListener(IPAddress.Parse("127.0.0.1"), 8000);
+            _listner.Start();
+
+
+            while (true)
+            {
+                
+                var client = new Client(_listner.AcceptTcpClient());
+                _users.Add(client);
+
+                //Broadcase the connection to everyone on the server
+            }  
+           
+            
+            //Console.WriteLine("Client Has Connected YAY");
+        }
+    }
+
+
+}
